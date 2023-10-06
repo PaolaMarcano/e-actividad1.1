2c0e7307ed858f6275efb6ad5b13c06ce92d8cce
Paola <marcanosalaspv@uvm.edu.ve>
2023-10-06 12:42:38 -0400

    Constructores a los models

--- models/Categoria_model.js
+++ models/Categoria_model.js
@@ -1,15 +1,32 @@
-const categorias = require('../categorias');
+const categorias = require('../ejemplos/categorias');
 const respuesta = require('../models/Respuesta')
 const { v4: uuidv4 } = require('uuid');
+
+class Categoria{
+    constructor(id, id_modalidad, nombre, descripcion, premio){
+        this.id = id,
+        this.id_modalidad = id_modalidad,
+        this.nombre = nombre,
+        this.descripcion = descripcion,
+        this.reglas = [],
+        this.premio = premio
+    }
+    agregar_reglas(regla){
+        this.reglas.push(regla);
+    }
+}
 
 class CategoriaModel{
     ingresar_categoria(categoria){ //verificar que los id de modalidad existan
         categoria.id = uuidv4();
-        categorias.push(categoria);
+        let nueva_categoria = new Categoria(categoria.id, categoria.id_modalidad, categoria.nombre, categoria.descripcion, categoria.premio);
+        for (let i = 0; i < categoria.reglas.length; i++) {
+            nueva_categoria.agregar_reglas(categoria.reglas[i])
+        }
+        categorias.push(nueva_categoria);
         let resultado = new respuesta(200, "categoria agregada con éxito", categorias); 
         return resultado;
     }
 }
-
 
 module.exports = new CategoriaModel();
--- models/Patrocinador_model.js
+++ models/Patrocinador_model.js
@@ -1,11 +1,23 @@
-const patrocinadores = require('../patrocinadores');
+const patrocinadores = require('../ejemplos/patrocinadores');
 const respuesta = require('../models/Respuesta');
 const { v4: uuidv4 } = require('uuid');
+
+class Patrocinador{
+    constructor(id, nombre_comercial, persona_de_contacto, telefono, patrocinio, comentario){
+        this.id = id,
+        this.nombre_comercial = nombre_comercial,
+        this.persona_de_contacto = persona_de_contacto,
+        this.telefono = telefono,
+        this.patrocinio = patrocinio,
+        this.comentario = comentario
+    }
+}
 
 class PatrocinadorModel{
     ingresar_patrocinador(patrocinador){
         patrocinador.id = uuidv4();
-        patrocinadores.push(patrocinador);
+        let nuevo_patrocinador = new Patrocinador(patrocinador.id, patrocinador.nombre_comercial, patrocinador.persona_de_contacto, patrocinador.telefono, patrocinador.patrocinio, patrocinador.comentario);
+        patrocinadores.push(nuevo_patrocinador);
         let resultado = new respuesta(200, "patrocinador agregado con éxito", patrocinadores); 
         return resultado;
     }
