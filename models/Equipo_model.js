9a6da194fb7ab59cc28f00914faf51db2ce48c72
Paola <marcanosalaspv@uvm.edu.ve>
2023-10-06 12:47:53 -0400

    Constructores faltantes

--- models/Equipo_model.js
+++ models/Equipo_model.js
@@ -1,14 +1,35 @@
-const equipos = require('../equipos');
+const equipos = require('../ejemplos/equipos');
 const respuesta = require('../models/Respuesta')
 const { v4: uuidv4 } = require('uuid');
+
+class Equipo{
+    constructor(id, representante, modalidad, email, telefono, nombre_del_equipo, comentario, categoria){
+        this.id = id,
+        this.representante = representante,
+        this.modalidad = modalidad,
+        this.email = email,
+        this.telefono = telefono,
+        this.nombre_del_equipo = nombre_del_equipo,
+        this.participantes = [],
+        this.comentario = comentario,
+        this.categoria = categoria
+    }
+    agregar_participantes(participante){
+        this.participantes.push(participante);
+    }
+}
 
 class EquipoModel{
     ingresar_equipo(equipo){
         equipo.id = uuidv4();
-        equipos.push(equipo);
+        let nuevo_equipo = new Equipo(equipo.id, equipo.representante, equipo.modalidad, equipo.email, equipo.telefono, equipo.nombre_del_equipo, equipo.comentario, equipo.categoria);
+        for (let i = 0; i < equipo.participantes.length; i++) {
+            nuevo_equipo.agregar_participantes(equipo.participantes[i])
+        }
+        equipos.push(nuevo_equipo);
         let resultado = new respuesta(200, "equipo agregado con éxito", equipos); 
         return resultado;
     }
 }
 
-module.exports = new EquipoModel();
+module.exports = new EquipoModel(); 
--- models/Modalidad_model.js
+++ models/Modalidad_model.js
@@ -1,11 +1,19 @@
-const modalidades = require('../modalidades');
+const modalidades = require('../ejemplos/modalidades');
 const respuesta = require('../models/Respuesta')
 const { v4: uuidv4 } = require('uuid');
+
+class Modalidad{
+    constructor(id, nombre){
+        this.id =  id,
+        this.nombre = nombre
+    }
+}
 
 class ModalidadModel{
     ingesar_modalidad(modalidad){
         modalidad.id = uuidv4();
-        modalidades.push(modalidad);
+        let nueva_modalidad = new Modalidad(modalidad.id, modalidad.nombre)
+        modalidades.push(nueva_modalidad);
         let resultado = new respuesta(200, "modalidad agregada con éxito", modalidades); 
         return resultado;
     }
